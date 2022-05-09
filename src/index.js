df02b004842d289ee23f038c76b24905d3ea421e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-08 19:28:30 -0700

    add import for Count - WIP w/note

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery'; // unrecognized
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Count from './js/Count.js'; // unrecognized
